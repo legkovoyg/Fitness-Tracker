a3528f4cd34c9c4c9279a18f4864d2773ee833b8
legkovoyg <147488090+legkovoyg@users.noreply.github.com>
2024-08-02 16:58:33 +0500

    qq

--- database/models.py
+++ database/models.py
@@ -10,8 +10,10 @@ load_dotenv()
 engine = create_async_engine(url=os.getenv("SQLALCHEMY_URL"))
 async_session = async_sessionmaker(engine)
 
+
 class Base(AsyncAttrs, DeclarativeBase):
     pass
+
 
 class User(Base):
     __tablename__ = "users"
@@ -21,7 +23,7 @@ class User(Base):
     name: Mapped[str] = mapped_column()
     weight: Mapped[float] = mapped_column()
     height: Mapped[float] = mapped_column()
-
+    gender: Mapped[int] = mapped_column()
 
 
 class Category(Base):
@@ -29,16 +31,34 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
 
+
 class Exercise(Base):
-    __tablename__ = 'exercises'
+    __tablename__ = "exercises"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
     category_id: Mapped[int] = mapped_column(BigInteger)
 
+
 class User_properties(Base):
     __tablename__ = "user_properties"
 
     tg_id = mapped_column(BigInteger)
-    Hip:Mapped[float] = mapped_column()
-    Hips:
+    Hip: Mapped[float] = mapped_column()  # бедро
+    Hips: Mapped[float] = mapped_column()  # бедра
+    Chest: Mapped[float] = mapped_column()  # Грудь
+    Neck: Mapped[float] = mapped_column()  # Шея
+    Waist: Mapped[float] = mapped_column()  # Талия
+    Forearm: Mapped[float] = mapped_column()  # Предплечье
+    Shin: Mapped[float] = mapped_column()  # Шея
+    Biceps: Mapped[float] = mapped_column()  # Бицепс
+    Brush: Mapped[float] = mapped_column()  # Кисть
+    weight: Mapped[float] = mapped_column()  # вес
+
+    date: Mapped[float] = mapped_column()  # дата
+
+
+class User_workout(Base):
+    __tablename__ = "user_workout"
+
+    tg_id = mapped_column(BigInteger)
